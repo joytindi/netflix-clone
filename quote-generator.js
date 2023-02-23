29cc3159a21bcda993641d503e51ea2a976ec53f
TINDI JOY <joytindi24@gmail.com>
2023-02-23 13:55:02 +0300

    text

---
+++ quote-generator.js
@@ -0,0 +1,9 @@
+@@ -132,6 +132,8 @ @ let quotes = {
+
+}
+
+// fetch Api
+
+let generateQuoteButton = document.querySelector(".generator-btn");
+let quoteContainer = document.querySelector(".quote-container");
+// console.log(quoteContainer);
